8ff7c44b7c3713dd3c98590375cb01d8ea7db4c7
Kaneda Shotaro <k4n3d4_sh0t4r0@proton.me>
2025-06-25 14:06:36 +0200

    Change package name to avoid incompatibilities

---
+++ src/main/java/org/k4n3d4/NoJumpDelay/NoJumpDelayModule.java
@@ -0,0 +1,59 @@
+package org.k4n3d4.NoJumpDelay;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import org.k4n3d4.NoJumpDelay.mixins.LivingEntityAccessor;
+import org.rusherhack.client.api.events.client.EventUpdate;
+import org.rusherhack.client.api.feature.module.ModuleCategory;
+import org.rusherhack.client.api.feature.module.ToggleableModule;
+import org.rusherhack.core.event.subscribe.Subscribe;
+import org.rusherhack.core.setting.NumberSetting;
+
+public class NoJumpDelayModule extends ToggleableModule{
+    public static boolean IsEnable = false;
+    public static final NumberSetting<Integer> delay = new NumberSetting<>("Delay", "Delay in ticks between touching ground and jumping",0, 0, 20);
+
+    public NoJumpDelayModule() {
+        super("NoJumpDelay", "Set a custom delay between jumps when holding space key", ModuleCategory.CLIENT);
+
+        this.registerSettings(
+                this.delay
+        );
+    }
+
+    private int jumpCooldown = 0;
+
+    @Subscribe
+    private void onUpdate(EventUpdate event) {
+        LocalPlayer player = Minecraft.getInstance().player;
+        LivingEntityAccessor accessor = (LivingEntityAccessor) player;
+
+        accessor.setNoJumpDelay(100);
+
+        if (jumpCooldown > 0) {
+            if (!player.input.keyPresses.jump()) {
+                jumpCooldown = 0;
+                accessor.setNoJumpDelay(0);
+            }
+            else {
+                jumpCooldown--;
+            }
+        }
+
+        if (player.input.keyPresses.jump() && player.onGround() && jumpCooldown == 0) {
+            accessor.setNoJumpDelay(0);
+            jumpCooldown = delay.getValue();
+        }
+    }
+
+
+    @Override
+    public void onEnable() {
+        IsEnable = true;
+    }
+
+    @Override
+    public void onDisable() {
+        IsEnable = false;
+    }
+}
---
+++ src/main/java/org/k4n3d4/NoJumpDelay/NoJumpDelayPlugin.java
@@ -0,0 +1,26 @@
+package org.k4n3d4.NoJumpDelay;
+
+import org.rusherhack.client.api.RusherHackAPI;
+import org.rusherhack.client.api.plugin.Plugin;
+
+public class NoJumpDelayPlugin extends Plugin {
+	
+	public static long minecraftInitTime = 0L;
+	public static NoJumpDelayPlugin INSTANCE;
+	
+	@Override
+	public void onLoad() {
+		INSTANCE = this;
+
+		final NoJumpDelayModule noJumpDelayModule = new NoJumpDelayModule();
+		RusherHackAPI.getModuleManager().registerFeature(noJumpDelayModule);
+
+		this.getLogger().info("Loading NoJumpDelay plugin");
+	}
+
+	@Override
+	public void onUnload() {
+		this.getLogger().info("NoJumpDelay plugin unloaded!");
+	}
+	
+}
